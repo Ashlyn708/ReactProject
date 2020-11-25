610c525310f21bd47ec6e49d6eb3a31cd74bba29
Ashlyn708 <averett_ashlyn@columbusstate.edu>
2020-11-25 21:46:00 +0000

    11/25 updates

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,17 +1,38 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TaskList from './TasksList';
 
-function App() {   
-  return (
-    <div className="App">
-      <h1>To-do List</h1>
-      <input></input>
-      <button>Add Todo</button>
-      <TaskList/>
-      <TaskList/>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            todoList: null
+        }
+    }
+    render(){
+        return (
+            <div className="App">
+                <h1>To-do List</h1>
+                <input></input>
+                <button onClick={(e)=>this.addTodo(e)}>Add Todo</button>
+                <TaskList title={'Pending ToDo'} buttonText={'Done'} tasks={this.state.todoList} buttonFunction={this.removeTodo}/>
+                <TaskList title={'Completed'} buttonText={'Delete'} tasks={'done'} buttonFunction={this.removeTodo}/>
+            </div>
+        );
+    }
+    
+    addTodo(e){
+        console.log(e);
+        this.setState({todoList:'item1'});
+    }
+
+    removeTodo(){
+        if(this.state.todoList!=null){
+                    this.setState({todoList:null})
+
+        }
+    }
 }
 
 export default App;
